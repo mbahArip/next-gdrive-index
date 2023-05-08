e1a0a4211293c721aba8863b5314c81e10c329d4
mbaharip <62494292+mbahArip@users.noreply.github.com>
2023-05-08 16:34:09 +0700

    Add pretty path to access media (image / video / audio)

---
+++ src/pages/media/[fileId].tsx
@@ -0,0 +1,69 @@
+import { GetServerSideProps } from "next";
+import { FileResponse } from "types/googleapis";
+import axios from "axios";
+import { urlDecrypt } from "utils/encryptionHelper";
+import { promisify } from "util";
+import { pipeline } from "stream";
+import driveClient from "utils/driveClient";
+import apiConfig from "config/api.config";
+
+export default function Media() {
+  return <></>;
+}
+
+const pipelineAsync = promisify(pipeline);
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { fileId } = context.query;
+
+  const searchFile = await axios.get<FileResponse>(
+    `${process.env.NEXT_PUBLIC_DOMAIN}/api/files/${fileId}`,
+  );
+  if (!searchFile.data.success) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const id = urlDecrypt(searchFile.data.file.id as string);
+  const fileName = searchFile.data.file.name as string;
+  const mimeType = searchFile.data.file.mimeType as string;
+
+  if (
+    !mimeType.startsWith("image/") &&
+    !mimeType.startsWith("video/") &&
+    !mimeType.startsWith("audio/")
+  ) {
+    return {
+      notFound: true,
+    };
+  }
+
+  if (Number(searchFile.data.file.size as string) > apiConfig.maxResponseSize) {
+    return {
+      redirect: {
+        destination: urlDecrypt(searchFile.data.file.webContentLink as string),
+        permanent: false,
+      },
+    };
+  }
+
+  const getImageStream = await driveClient.files.get(
+    {
+      fileId: id,
+      alt: "media",
+    },
+    { responseType: "stream" },
+  );
+
+  context.res.setHeader("Content-Type", mimeType);
+  context.res.setHeader(
+    "Content-Disposition",
+    `inline; filename="${fileName}"`,
+  );
+
+  await pipelineAsync(getImageStream.data, context.res);
+
+  return {
+    props: {},
+  };
+};
