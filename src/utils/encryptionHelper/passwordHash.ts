e5dd25205e1b6c2e9b3325fcdca88d556ce5057f
mbaharip <62494292+mbahArip@users.noreply.github.com>
2023-05-25 20:34:18 +0700

    Add function to comparing stored password and actual password

--- src/utils/encryptionHelper/passwordHash.ts
+++ src/utils/encryptionHelper/passwordHash.ts
@@ -8,9 +8,14 @@ function verify(text: string, hash: string): boolean {
   return encode(text) === hash;
 }
 
+function compare(hash1: string, hash2: string): boolean {
+  return hash1 === hash2;
+}
+
 const passwordHash = {
   encode,
   verify,
+  compare,
 };
 
 export default passwordHash;
