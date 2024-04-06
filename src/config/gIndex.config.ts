7aea7703f67e4280ef5b407c26b854f45c0c3df0
mbaharip <62494292+mbahArip@users.noreply.github.com>
2024-04-06 22:24:36 +0700

    Set `NEXT_PUBLIC_DOMAIN` as a fallback of `NEXT_PUBLIC_VERCEL_URL`

--- src/config/gIndex.config.ts
+++ src/config/gIndex.config.ts
@@ -17,7 +17,9 @@ const config: z.input<typeof Schema_Config> = {
   basePath:
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : `https://${process.env.NEXT_PUBLIC_DOMAIN}`,
+      : `https://${
+          process.env.NEXT_PUBLIC_VERCEL_URL || process.env.NEXT_PUBLIC_DOMAIN
+        }`,
 
   /**
    * DEPRECATED
