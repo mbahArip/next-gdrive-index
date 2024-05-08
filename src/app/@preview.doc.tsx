5dd73903f0d6e0fc798385077a0f396d5be9fd2f
mbaharip <62494292+mbahArip@users.noreply.github.com>
2024-05-08 09:49:24 +0700

    Add view document button on document preview

--- src/app/@preview.doc.tsx
+++ src/app/@preview.doc.tsx
@@ -7,19 +7,38 @@ import { Schema_File } from "~/schema";
 import { cn } from "~/utils";
 
 import Icon from "~/components/Icon";
+import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert";
 
 import config from "~/config/gIndex.config";
 
 import { CreateDownloadToken } from "./actions";
 
 const GoogleDocsViewerRenderer: DocRenderer = ({
-  mainState: { currentDocument },
+  mainState: { currentDocument, documentLoading },
 }) => {
   if (!currentDocument || !currentDocument.uri) return null;
 
   const viewerUrl = new URL(`/gview`, "https://docs.google.com");
   viewerUrl.searchParams.set("url", currentDocument.uri);
   viewerUrl.searchParams.set("embedded", "true");
+
+  if (documentLoading) {
+    return (
+      <div
+        className={cn(
+          "h-auto min-h-[50dvh] w-full",
+          "flex flex-grow flex-col items-center justify-center gap-3",
+        )}
+      >
+        <Icon
+          name='LoaderCircle'
+          size={32}
+          className='animate-spin text-foreground'
+        />
+        <p>Loading document...</p>
+      </div>
+    );
+  }
 
   return (
     <iframe
@@ -104,61 +123,79 @@ export default function PreviewDoc({ file }: Props) {
           <span className='text-center text-destructive'>{error}</span>
         </div>
       ) : (
-        <DocViewer
-          key={file.encryptedId}
-          documents={[
-            {
-              uri: docSrc,
-              fileName: file.name,
-              fileType: file.mimeType,
-            },
-          ]}
-          pluginRenderers={[GoogleDocsViewerRenderer]}
-          config={{
-            header: {
-              disableHeader: true,
-            },
-            loadingRenderer: {
-              overrideComponent: () => (
-                <div
-                  className={cn(
-                    "h-auto min-h-[50dvh] w-full",
-                    "flex flex-grow flex-col items-center justify-center gap-3",
-                  )}
-                >
-                  <Icon
-                    name='LoaderCircle'
-                    size={32}
-                    className='animate-spin text-foreground'
-                  />
-                  <p>Loading document...</p>
-                </div>
-              ),
-              showLoadingTimeout: 10000,
-            },
-            noRenderer: {
-              overrideComponent: () => (
-                <div className='flex h-full flex-col items-center justify-center gap-3'>
-                  <Icon
-                    name='CircleX'
-                    size={24}
-                    className='text-destructive'
-                  />
-                  <span className='text-center text-destructive'>
-                    Error loading document
-                  </span>
-                </div>
-              ),
-            },
-          }}
-          className={cn(
-            "h-full max-h-[70dvh] min-h-[70dvh] w-full overflow-hidden rounded-[var(--radius)] !text-black",
-            "rounded-[var(--radius)] [&>div#proxy-renderer]:overflow-hidden ",
-          )}
-          theme={{
-            disableThemeScrollbar: true,
-          }}
-        />
+        <div className='h-fit w-full space-y-3 overflow-hidden rounded-[var(--radius)]'>
+          <DocViewer
+            key={file.encryptedId}
+            documents={[
+              {
+                uri: docSrc,
+                fileName: file.name,
+                fileType: file.mimeType,
+              },
+            ]}
+            pluginRenderers={[GoogleDocsViewerRenderer]}
+            config={{
+              header: {
+                disableHeader: true,
+              },
+              loadingRenderer: {
+                overrideComponent: () => (
+                  <div
+                    className={cn(
+                      "h-auto min-h-[50dvh] w-full",
+                      "flex flex-grow flex-col items-center justify-center gap-3",
+                    )}
+                  >
+                    <Icon
+                      name='LoaderCircle'
+                      size={32}
+                      className='animate-spin text-foreground'
+                    />
+                    <p>Loading document...</p>
+                  </div>
+                ),
+                showLoadingTimeout: 10000,
+              },
+              noRenderer: {
+                overrideComponent: () => (
+                  <div className='flex h-full flex-col items-center justify-center gap-3'>
+                    <Icon
+                      name='CircleX'
+                      size={24}
+                      className='text-destructive'
+                    />
+                    <span className='text-center text-destructive'>
+                      Error loading document
+                    </span>
+                  </div>
+                ),
+              },
+            }}
+            className={cn(
+              "h-full max-h-[70dvh] min-h-[70dvh] w-full overflow-hidden rounded-[var(--radius)] !text-black",
+              "rounded-[var(--radius)] [&>div#proxy-renderer]:overflow-hidden ",
+            )}
+            theme={{
+              disableThemeScrollbar: true,
+            }}
+          />
+
+          <Alert className='bg-yellow-50 text-yellow-600 dark:bg-yellow-950 dark:text-yellow-500'>
+            <div className='flex items-start gap-3'>
+              <Icon
+                name='TriangleAlert'
+                className='size-5'
+              />
+              <div className='flex flex-col'>
+                <AlertTitle>Preview Only</AlertTitle>
+                <AlertDescription>
+                  It might failed to load the document, you can try to refresh
+                  the page or download the document to view it.
+                </AlertDescription>
+              </div>
+            </div>
+          </Alert>
+        </div>
       )}
     </div>
   );
