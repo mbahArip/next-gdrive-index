9b42865237966775b0277c2b9698867555fe0867
mbaharip <62494292+mbahArip@users.noreply.github.com>
2025-01-24 17:42:26 +0700

    update: rename tsx to ts

--- src/components/Layout/index.ts
+++ src/components/Layout/index.ts
@@ -1,5 +1,7 @@
+export { default as ErrorComponent } from "./Error";
 export { default as Footer } from "./Footer";
 export { default as Navbar } from "./Navbar";
+export { default as NotFoundComponent } from "./NotFound";
 export { default as PageLoader } from "./PageLoader";
 export { default as Password } from "./Password";
 export { default as Provider } from "./Provider";
