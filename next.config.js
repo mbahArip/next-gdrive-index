4dce74c506be20350406c8970048bbba2d81ad5b
mbaharip <62494292+mbahArip@users.noreply.github.com>
2025-01-24 18:12:10 +0700

    fix: add webpack resolve alias, hoping it would fix can't resolve alias path

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /* eslint-disable */
+const path = require("path");
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ["tsx", "ts"],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "~": path.resolve(__dirname, "./"),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
